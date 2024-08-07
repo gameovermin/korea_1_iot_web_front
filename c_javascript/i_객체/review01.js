e46724d92efe5714a2a7b350535aa50771334a07
gameovermin <qorkrhvms@gmail.com>
2024-08-07 10:15:31 +0900

    임시커밋

--- c_javascript/i_객체/review01.js
+++ c_javascript/i_객체/review01.js
@@ -1,3 +1,17 @@
 // ! Todo 리스트 만들기(할 일 목록 관리 시스템)
 
 
+// Todo 객체 구성
+// - id : 각 할 일의 고유 식별자, 주로 숫자나 문자열로 표현
+// - content : 할 일의 내용을 문자열로 저장
+// - completed : 할 일의 완료 상태를 나타내는 boolean 데이터
+
+let todo = {
+  id : 1,
+  content : 'sqld 공부하기',
+  completed : false
+}
+
+// # 프로젝트 구조
+
+// 1. 할일 목록을 저장
