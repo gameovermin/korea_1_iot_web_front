c0043192cb08c8815104a5201dfc8fc0f404fdbb
gameovermin <qorkrhvms@gmail.com>
2024-08-07 10:06:25 +0900

    20240807 오전 임시커밋

--- c_javascript/i_객체/practice01.js
+++ c_javascript/i_객체/practice01.js
@@ -20,10 +20,13 @@ console.log(fruits[1]);
 // 3. 함수 정의
 // : 두 개의 숫자를 매개변수로 받아 그 합을 반환하는 함수 add를 작성
 
-const add = (num1, num2) => num1 * num2; 
+const add = (num1, num2) => num1 + num2; 
+
+console.log(add(3, 5));
 
 
 //? 문제 2
+console.log('=== 문제 2번 ===');
 // 1. 객체 탐색
 // : 초급에서 작성한 person 객체의 모든 속성과 값을 순회하며 콘솔에 출력하는 코드를 작성
 // >> for in 반복문 사용
@@ -52,6 +55,20 @@ for (let per in person) {
 // 2. 배열 메서드 사용
 // : 초급에서 작성한 fruits의 모든 과일을 대문자로 변환하여 새 배열에 저장하고, 이 배열을 콘솔에 출력
 
+// - forEach : 반환 X / 전체요소에 동일한 기능을 적용
+// - map : 새로운 배열을 반환 / 전체 요소에 동일한 기능을 적용
+// - filter : 새로운 배열을 반환 / 해당 조건식을 참으로 통과하는 요소만 새로운 배열로 반환
+
+// +) 대문자 변환 : 문자열.toUpperCase()
+
+// 배열 메서드에서 쓰이는 콜백함수의 인자 (value, index, array)
+
+let upperCaseFruits = fruits.map(function (value, index, array) {
+  return value.toUpperCase();
+});
+
+console.log(upperCaseFruits);
+
 const upperFruits = fruits.map(fruit => fruit.toUpperCase());
 
 console.log(upperFruits);
@@ -59,16 +76,53 @@ console.log(upperFruits);
 // 3. 함수 활용
 // : 두 개의 배열을 매개변수로 받아, 첫 번째 배열의 모든 요소에 두 번째 배열의 요소를 순서대로 더한 새 배열을 반환하는 함수를 작성
 
+function combined(arr1, arr2) {
+  let result = arr1.map((elem, idx) => {
+    return elem + arr2[idx];    // 동일한 인덱스 번호를 가진 요소끼리 더해져서 반환
+  });
+  return result;
+}
+
+console.log(combined([1, 2, 3, 4], [5, 6, 7, 8]));
+
+
 function combinedArrays(arr1, arr2) {
   return arr1.map((elem, idx) => elem + arr2[idx]);
 }
+
+console.log(combinedArrays([1, 2, 3], [4, 5, 6]));
+
+function combinedArrays2(arr1, arr2, arr3) {
+  return arr1
+      .map((elem, idx) => elem + arr2[idx])
+      .map((elem, idx) => elem + arr3[idx]);
+}
+
+console.log(combinedArrays2([1,2,3], [4,5,6], [7,8,9]));
+
 // [1, 2, 3]
 // [4, 5, 6]
 // >> [5, 7, 9]
 
+console.log('=== 문제 3 ===');
+
 //? 문제 3
 // 1. 객체 깊은 복사
 // : person 객체를 깊은 복사하는 함수를 작성
+
+// +) 객체의 깊은 복사 JSON 자료형을 사용
+// >> JSON.stringify()
+
+function deepCopy(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+let personCopy = deepCopy(person);
+
+person.name = 'ronaldo';
+
+console.log(person);  //  { name: 'ronaldo', age: 22, isStudent: false }
+console.log(personCopy);  //  { name: '벨링엄', age: 22, isStudent: false }
 
 // 2. 배열과 객체를 활용한 데이터 처리
 // : 아래의 users 배열에서 성인 사용자(18세 이상)만 필터링하고, 필터링된 사용자의 이름을 새 배열로 만들어 반환하는 함수를 작성
@@ -79,3 +133,14 @@ const users = [
   { name: "Jane", age: 17 },
   { name: "Doe", age: 18 }
 ];
+
+function adultUserName(users) {
+  // return users.filter((user) => {
+  //   return user.age >= 18
+  // });
+  return users
+      .filter(user => user.age >= 18) //  users 배열에서 성인 사용자(18세 이상)만 필터링
+      .map(user => user.name);  //  필터링된 사용자의 이름을 새 배열로 만들어 반환
+}
+
+console.log(adultUserName(users));
